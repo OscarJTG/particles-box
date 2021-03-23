181f917d813109761bc0ebd033619a1947f7d7a9
OscarJTG <78657968+OscarJTG@users.noreply.github.com>
2021-03-23 08:03:23 +0000

    Added collisions with container wall
    Imports a function from the collision.py file.
    
    Also times how long the calculation takes.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import numpy as np 
 from numpy import random
 import matplotlib.pyplot as plt
+from collision import collisionWall
+import time
 
 
 # Declare some values.
-N = 100 # Number of particles.
+N = 1000 # Number of particles.
 N_dim = 3 # Number of dimensions.
 box_side = 2 # Side length of box.
 v0 = 1 # Velocity scale factor.
-dt = 0.1 # Time step.
+dt = 0.02 # Time step.
+T = 1 # Total simulation time.
+N_it = int(T/dt) # Number of iteration steps.
 
 # Random array of initial particle positions,
 # such that particles lie within box centred at (0,0,0).
@@ -20,7 +24,7 @@ v = random.rand(N, 3) * v0 - 0.5*v0
 #print(x)
 #print(v)
 
-def update_positions(x0, v0, dt):
+def updatePositions(x0, v0, dt):
     """
     Calculates the positions after time dt, given particle positions and velocities.
 
@@ -38,13 +42,25 @@ def update_positions(x0, v0, dt):
 
 
 
-
+# Plot particle positions at t=0.
 fig, ax = plt.subplots(figsize = (5,10))
 ax.plot(x[:,0], x[:,1], 'ko', ms = 2.0)
 
-for i in range(10):
-	x = update_positions(x,v,dt)
+
+tic = time.process_time() # For timing calculation time.
+
+# Calculate subsequent positions and velocities.
+for i in range(N_it):
+    x = updatePositions(x, v, dt)
+    v = collisionWall(x, v, box_side)
 
 
+toc = time.process_time()
+print("Calculation time =", (toc-tic), "s")
+print("Number of particles =", N)
+print("Number of time steps =", N_it)
+
+# Plot particle positions at the end of simulation 
+# (check they remain inside box).
 ax.plot(x[:,0], x[:,1], 'bo', ms = 2.0)
 plt.show()
